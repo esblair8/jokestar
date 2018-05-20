d3b1a143f20e0b5efda05bb095ca7b3cf3e19c74
Evan Blair <esblair8@gmail.com>
2018-05-20 12:03:58 +0100

    simple vuejs application that will show different jokes

---
+++ src/components/App.vue
@@ -0,0 +1,46 @@
+<template>
+	<div id="app">
+		<h3>Got Jokes?</h3>
+		<button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+		<button class="btn btn-primary" @click="addJoke">Add A Joke</button>
+		<br>
+		<span v-for="type in types">
+			<input type="checkbox" 
+			:value="type" 
+			v-model="checkedTypes"
+			checked>
+			<label>{{type}}</label>&nbsp;
+		</span>
+		<br>
+		<div class="col-md-6">
+			<Joke
+				v-for="(joke, index) in $store.state.jokes"
+				v-show="checkedTypes.includes(joke.type)"
+				v-bind:joke="joke"
+				v-bind:index="index"
+				v-bind:key="index"	
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { mapActions } from 'vuex'
+	import Joke from './Joke.vue'
+
+	export default {
+		data() {
+			return {
+				types: ['general', 'knock-knock', 'programming'],
+				checkedTypes: ['general', 'knock-knock', 'programming']
+			}
+		},
+		methods: mapActions([
+			'initJokes',
+			'addJoke'
+		]),
+		components: {
+			Joke
+		}
+	}
+</script>
---
+++ src/components/Joke.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="col-md-6">
+		<div class="joke-card">
+			<div class="card-block">
+				<button class="close" @click="removeJoke(index)"><span>&times;</span></button>
+				<p><u>{{ joke.setup }}</u></p>
+				<p><em>{{ joke.punchline }}</em></p>
+			</div>		
+		</div>
+	</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+	props: ['joke', 'index'],
+	methods: mapActions([
+		'removeJoke'
+	])
+}
+</script>
+
+<style>
+
+.joke-card {
+	border: 2px solid #2C3350;
+	border-radius: 2px;
+	padding: 5px;
+	margin: 5px;
+}
+</style>
